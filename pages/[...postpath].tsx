2d2aa2f1490dcd4c53bf14ac820362664f3765a3
antilravi22 <114747820+antilravi22@users.noreply.github.com>
2023-05-20 22:36:31 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -27,7 +27,16 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	const query = gql`
 		{
 			post(id: "http://www.digitalnagar.org.in" + encodeURI(path as string, idType: URI) {
-				 {
+	id
+				excerpt
+				title
+				link
+				dateGmt
+				modifiedGmt
+				content
+				author	
+
+			 {
 					node {
 						name
 					}
@@ -76,13 +85,7 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
-				<meta property="og:title" content={post.title} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
-				<meta property="og:type" content="article" />
-				<meta property="og:locale" content="en_US" />
-				<meta property="og:site_name" content={host.split('.')[0]} />
-				<meta property="article:published_time" content={post.dateGmt} />
-				<meta property="article:modified_time" content={post.modifiedGmt} />
+				
 				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
 				<meta
 					property="og:image:alt"
